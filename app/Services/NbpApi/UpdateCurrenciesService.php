962e22a95f63160624705599608dc164e86feccb
PiotrZapolski <zapol1230@gmail.com>
2023-04-15 15:07:54 +0200

    better eloquent

--- app/Services/NbpApi/UpdateCurrenciesService.php
+++ app/Services/NbpApi/UpdateCurrenciesService.php
@@ -18,18 +18,10 @@ class UpdateCurrenciesService
     {
         try {
             foreach($data->rates as $rate){
-                $currency = Currency::where('currency_code', $rate->code)->first();
-                if ($currency === null) {
-                    Currency::create([
-                        'name' => $rate->currency,
-                        'currency_code' => $rate->code,
-                        'exchange_rate' => $rate->mid,
-                    ]);
-                }
-                else{
-                    $currency->exchange_rate = $rate->mid;
-                    $currency->save();
-                }
+                Currency::updateOrCreate(
+                    ['currency_code' => $rate->code],
+                    ['name' => $rate->currency, 'exchange_rate' => $rate->mid]
+                );
             }
         }
         catch (\Exception $e) {
